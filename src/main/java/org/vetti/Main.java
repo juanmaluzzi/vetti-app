5cbeaf88fb4ed37e34607f867fc3db67e135836e
juanmaluzzi <juaanma.-97@hotmail.com>
2024-09-08 22:23:04 -0300

    wip

--- src/main/java/org/vetti/Main.java
+++ src/main/java/org/vetti/Main.java
@@ -10,15 +10,13 @@ public class Main {
     public static void main(String[] args) {
         Dotenv dotenv = null;
 
-        // Solo cargar el archivo .env si estamos en un entorno local (desarrollo)
         if (System.getenv("ENVIRONMENT") == null || System.getenv("ENVIRONMENT").equals("development")) {
-            dotenv = Dotenv.load();  // Cargar el .env en local
+            dotenv = Dotenv.load();  // Cargar .env en local
             System.out.println("Cargando variables desde .env en entorno de desarrollo");
         } else {
             System.out.println("Usando variables de entorno configuradas en producción");
         }
 
-        // Establecer propiedades del sistema ya sea desde dotenv (local) o desde System.getenv (prod)
         setSystemProperty("AUTH0_CLIENT_ID", dotenv);
         setSystemProperty("AUTH0_CLIENT_SECRET", dotenv);
         setSystemProperty("AUTH0_ISSUER_URI", dotenv);
@@ -32,10 +30,10 @@ public class Main {
 
     private static void setSystemProperty(String key, Dotenv dotenv) {
         String value = (dotenv != null) ? dotenv.get(key) : System.getenv(key);
-        if (value != null) {
+        if (value != null && !value.isEmpty()) {
             System.setProperty(key, value);
         } else {
-            System.out.println("Warning: Variable de entorno " + key + " no está definida.");
+            System.out.println("Advertencia: Variable de entorno " + key + " no está definida.");
         }
     }
 }
