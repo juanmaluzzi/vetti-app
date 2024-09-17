db7fb3c36f08f29e9e04b74cfa90698240dd2bf0
juanmaluzzi <juaanma.-97@hotmail.com>
2024-09-17 19:44:47 -0300

    wip

--- src/main/java/org/vetti/controller/UserController.java
+++ src/main/java/org/vetti/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
