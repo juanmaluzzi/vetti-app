701f36678da9c72e5a588402adcf22c2663e714d
juanmaluzzi <juaanma.-97@hotmail.com>
2024-10-16 20:45:22 -0300

    wip

--- src/main/java/org/vetti/controller/UserController.java
+++ src/main/java/org/vetti/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "Authorization")
 public class UserController {
 
     @Autowired
